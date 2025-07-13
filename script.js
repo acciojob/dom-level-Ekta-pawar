857b29bf0b5d656b2a2c0033956c0daa8153e915
Ekta-pawar <ektapawar0001@gmail.com>
2025-07-13 21:08:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function getDomLevel(element) {
+      let level = 0;
+      let current = element;
+
+      // Keep going up until you reach <html> (which has no parentElement)
+      while (current.parentElement) {
+        level++;
+        current = current.parentElement;
+      }
+
+      alert("The level of the element is: " + level);
+    }
+
